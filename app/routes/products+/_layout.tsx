d5b2e0755f1e9859c61ad2ce80990f6a6594af00
Serey Roth <sereyratanakroth@gmail.com>
2023-04-11 15:31:12 -0700

    feat(products+/_layout): add cart badge to Navbar

--- app/routes/products+/_layout.tsx
+++ app/routes/products+/_layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "@remix-run/react";
 import Navbar from "./nav-bar";
+import { Cart } from "./cart";
 
 export default function ProductsLayout() {
     return (
         <div className="flex flex-col bg-teal-500 min-h-screen w-screen
         px-2">
-            <div className="flex items-center w-full py-2">
+            <div className="flex items-center w-full py-2 gap-2">
                 <h1 className="font-bold text-lg mr-auto">Fake Store</h1>
                 <Navbar 
                 links={[
@@ -18,6 +19,7 @@ export default function ProductsLayout() {
                         to: "/products"
                     }
                 ]} />
+                <Cart />
             </div>
             <Outlet />
         </div>
