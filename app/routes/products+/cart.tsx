582619a18bef0e0a3fb4983e4bd27cf1257e248e
Serey Roth <sereyratanakroth@gmail.com>
2023-04-11 17:54:57 -0700

    feat($productsId+/cart): create buttons for adding items to cart
    Create a component that handles adding new items to
    the cart.

--- app/routes/products+/cart.tsx
+++ app/routes/products+/cart.tsx
@@ -1,8 +1,24 @@
-import type { LoaderArgs} from "@remix-run/node";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node"
-import { useFetcher } from "@remix-run/react";
-import { useEffect } from "react";
+import { useFetcher, useParams } from "@remix-run/react";
+import { useEffect, useState } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsFillCartCheckFill } from "react-icons/bs";
+
+export const action = async ({ request }: ActionArgs) => {
+    const form = await request.formData();
+    const productId = form.get("productId");
+    const quantity = form.get("quantity");
+
+    if (
+        typeof productId !== "string" ||
+        typeof quantity !== "string"
+    ) {
+        throw new Error("Form not submitted correctly!");
+    }
+
+    
+}
 
 export const loader = async ({ request }: LoaderArgs) => {
     return json({
@@ -29,6 +45,61 @@ export function Cart() {
         drop-shadow-sm">
             <AiOutlineShoppingCart size={20}/>
             <p className="text-sm">{fetcher.data?.cart}</p>
+        </div>
+    )
+}
+
+export function AddToCartButtons() {
+    const { productId } = useParams();
+
+    console.log(productId)
+
+    const [quantity, setQuantity] = useState(0);
+
+    const handleIncrement = () => {
+        setQuantity(quantity + 1);
+    }
+
+    const handleDecrement = () => {
+        if (quantity <= 0) {
+            return;
+        }
+        setQuantity(quantity - 1);
+    }
+
+    return (
+        <div className="flex items-center">
+            <button 
+            onClick={handleIncrement}
+            className="bg-white border 
+            rounded-l-full px-2">
+                +
+            </button>
+            <p className="bg-white border px-4 font-medium">
+                {quantity}
+            </p>
+            <button 
+            onClick={handleDecrement}
+            className="bg-white border px-2
+            rounded-r-full"
+            disabled={quantity === 0}>
+                -
+            </button>
+            <form method="post" action="/products/cart">
+                <input
+                type="hidden"
+                value={productId ? Number.parseInt(productId) : undefined}
+                name="productId" />
+                <input
+                type="hidden"
+                value={quantity}
+                name="quantity" />
+                <button 
+                type="submit"
+                className="ml-2 py-1 px-2 rounded-full bg-teal-600 text-gray-100">
+                    <BsFillCartCheckFill size={20}/>
+                </button>
+            </form>
         </div>
     )
 }
