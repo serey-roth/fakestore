8dc5a3493f7662e9dd8be8049c45a7e3b6a559e0
Serey Roth <sereyratanakroth@gmail.com>
2023-04-14 18:24:32 -0700

    feat(auth.server): set up register function
    Create a function that registers a user given the registration
    payload. The registration fails if passwords do not match,
    or the credentials determined by the payload are already taken.

--- app/routes/auth+/auth.server.ts
+++ app/routes/auth+/auth.server.ts
@@ -11,7 +11,14 @@ const UsernamePasswordSchema = z.object({
     password: z.string().min(MIN_PASSWORD_LENGTH, "Password must be at least 6 characters long!"),
 });
 
+const UserRegistrationSchema = UsernamePasswordSchema.extend({
+    email: z.string().email("Invalid email!"),
+    confirmPassword: z.string().min(MIN_PASSWORD_LENGTH, "Password must be at least 6 characters long!"),
+});
+
 type UsernamePassword = z.infer<typeof UsernamePasswordSchema>;
+
+type UserRegistrationArgs = z.infer<typeof UserRegistrationSchema>;
 
 type FieldErrors<TFields> = {
     [T in keyof TFields]?: string[];
@@ -63,9 +70,29 @@ const handleNoExistingUser = <TFields>(
     });
 }
 
+const handleExistingUser = <TFields>(
+    fields: TFields,
+    message = "Credentials already taken! Please try again."
+) => {
+    return handleBadRequest({
+        fields,
+        formError: message,
+    });
+}
+
 const handleIncorrectPassword = <TFields>(
     fields: TFields,
     message = "Incorrect password! Please try again."
+) => {
+    return handleBadRequest({
+        fields,
+        formError: message,
+    });
+}
+
+const handleMismatchedPasswords = <TFields>(
+    fields: TFields,
+    message = "Passwords do not match! Please try again."
 ) => {
     return handleBadRequest({
         fields,
@@ -112,5 +139,49 @@ export const login = async ({
     return handleOKRequest({
         id: user.id,
         username: user.username
+    });
+}
+
+export const register = async (
+    args: UserRegistrationArgs
+): Promise<{
+    data: Pick<User, "id" | "username"> | null,
+    errors: FormErrors<UserRegistrationArgs> | null, 
+}> => {
+    const validationResult = UserRegistrationSchema.safeParse(args);
+
+    if (!validationResult.success) {
+        const fieldErrors = validationResult.error.flatten().fieldErrors;
+        return handleInvalidFormFieldValues({
+            fields: args,
+            fieldErrors,
+        });
+    }
+
+    if (args.password !== args.confirmPassword) {
+        return handleMismatchedPasswords(args);
+    }
+
+    const user = await db.user.findUnique({
+        where: { username: args.username },
+    });
+
+    if (user) {
+        return handleExistingUser(args);
+    }
+
+    const passwordHash = await bcrypt.hash(args.password, 10);
+
+    const newUser = await db.user.create({
+        data: {
+            username: args.username,
+            email: args.email,
+            password: passwordHash,
+        }
+    });
+
+    return handleOKRequest({
+        id: newUser.id,
+        username: newUser.username,
     });
 }
